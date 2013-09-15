f019cfa09c9a58f4f85d0aa947dc9a55aefe1910
Takashi Toyofuku <takasing@takasing-mac.local>
2013-09-15 16:33:43 +0900

    make cart empty

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -65,9 +65,12 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
+    @cart = current_cart
     @cart.destroy
+    session[:cart_id] = nil
+
     respond_to do |format|
-      format.html { redirect_to carts_url }
+      format.html { redirect_to store_url, notice: 'カートは現在空です' }
       format.json { head :no_content }
     end
   end
--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -41,7 +41,7 @@ class CartsControllerTest < ActionController::TestCase
 
   test "should destroy cart" do
     assert_difference('Cart.count', -1) do
-      delete :destroy, id: @cart
+      delete :destroy, {id: @cart.to_param}, {cart_id: @cart_id}
     end
 
     assert_redirected_to carts_path
