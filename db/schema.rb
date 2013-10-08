a7421425b0b0e195b45cee126ddfe1b3955e07ca
takasing <takaty2011@gmail.com>
2013-10-08 09:32:45 +0900

    Revert "'$ rails generate migration add_order_id_to_line_item order_id:integer', '$ rake db:migrate'"
    This reverts commit 176bf513dfb5f4670a41a0666bed6b625241d0d4.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131005132232) do
+ActiveRecord::Schema.define(version: 20130914093708) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -24,17 +24,6 @@ ActiveRecord::Schema.define(version: 20131005132232) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "quantity",   default: 1
-    t.integer  "order_id"
-  end
-
-  create_table "orders", force: true do |t|
-    t.string   "name"
-    t.string   "address"
-    t.string   "text"
-    t.string   "email"
-    t.string   "pay_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "products", force: true do |t|
